ec318e9a979e4f185d416a47d04de813e2aed1ba
Dorota Gil <gildota@wp.pl>
2020-09-01 00:20:15 +0200

    Update AppModule

--- car-themed-app-frontend/src/app/app.module.ts
+++ car-themed-app-frontend/src/app/app.module.ts
@@ -1,18 +1,24 @@
+import { DealersService } from '@service/dealers.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DealersComponent } from '@components/dealers/dealers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DealersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
   ],
-  providers: [],
+  providers: [DealersService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
