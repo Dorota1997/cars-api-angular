0442f5577342380d211dfc114f0c60ac2e7c3a2d
Dorota Gil <gildota@wp.pl>
2020-09-04 22:49:23 +0200

    Submit data in BehaviorSubject

--- car-themed-app-frontend/src/app/components/dealers/dealers.component.ts
+++ car-themed-app-frontend/src/app/components/dealers/dealers.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { DealersService } from '@service/dealers.service';
+import { BehaviorSubject } from 'rxjs';
+import { IDealer } from '@model/dealer.model';
 
 @Component({
   selector: 'app-dealers',
   templateUrl: './dealers.component.html',
-  styleUrls: ['./dealers.component.less']
+  styleUrls: ['./dealers.component.less'],
 })
 export class DealersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  tableCols = ['id', 'name', 'address', 'country', 'postalCode'];
+  dealers = new BehaviorSubject<IDealer[]>([]);
+  constructor(private dealersService: DealersService) {
+    this.showDealers();
   }
 
+  ngOnInit() {}
+
+  showDealers() {
+    this.dealersService.dealers().subscribe((res) => {
+      this.dealers.next(res.data);
+    });
+  }
 }
