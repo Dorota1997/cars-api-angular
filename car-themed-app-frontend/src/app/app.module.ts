388ceb854905301c95c947936da25aee884137bd
Dorota Gil <gildota@wp.pl>
2020-09-04 22:47:10 +0200

    Update app.module.ts

--- car-themed-app-frontend/src/app/app.module.ts
+++ car-themed-app-frontend/src/app/app.module.ts
@@ -1,24 +1,43 @@
-import { DealersService } from '@service/dealers.service';
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { MatTableComponent } from '@components/mat-table/mat-table.component';
+import { BrowserModule, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DealersComponent } from '@components/dealers/dealers.component';
-
+import { APP_BASE_HREF } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatSortModule } from '@angular/material/sort';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTableModule } from '@angular/material/table';
+import { A11yModule } from '@angular/cdk/a11y';
 @NgModule({
   declarations: [
     AppComponent,
-    DealersComponent
+    DealersComponent,
+    MatTableComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
+    MatFormFieldModule
   ],
-  providers: [DealersService],
+  exports: [
+    A11yModule
+  ],
+  schemas: [NO_ERRORS_SCHEMA],
+  providers: [
+    // { provide: APP_BASE_HREF, useValue: '/' },
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
