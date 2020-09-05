03046a24561264ccef8105857342ade8be4968f8
Dorota Gil <gildota@wp.pl>
2020-09-05 18:55:14 +0200

    Create and implement orders component

---
+++ car-themed-app-frontend/src/app/components/orders/orders.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { OrdersService } from '@service/orders.service';
+import { BehaviorSubject } from 'rxjs';
+import { IOrder } from '@model/order.model';
+
+@Component({
+  selector: 'app-orders',
+  templateUrl: './orders.component.html',
+  styleUrls: ['./orders.component.less']
+})
+export class OrdersComponent implements OnInit {
+  tableCols = ['components', 'orderDate', 'dealer'];
+  orders = new BehaviorSubject<IOrder[]>([]);
+  title = 'Orders';
+  constructor(private ordersService: OrdersService) {
+    this.showOrders();
+  }
+
+  ngOnInit() {}
+
+  showOrders() {
+    this.ordersService.orders().subscribe((res) => {
+      this.orders.next(res.data);
+    });
+  }
+}
