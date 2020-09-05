a56b363c63fbac4f072135bf7d26caf276224648
Dorota Gil <gildota@wp.pl>
2020-09-05 19:00:55 +0200

    Add orders to routes

--- car-themed-app-frontend/src/app/app-routing.module.ts
+++ car-themed-app-frontend/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
+import { OrdersComponent } from '@components/orders/orders.component';
 import { DealersComponent } from '@components/dealers/dealers.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 
 const routes: Routes = [
   {
     path: 'dealers',
     component: DealersComponent,
+  },
+  {
+    path: 'orders',
+    component: OrdersComponent,
   },
   {
     path: '**',
