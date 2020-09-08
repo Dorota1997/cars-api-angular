cd5bf420b156fd4455b83ba91671850256ea19e2
Dorota Gil <gildota@wp.pl>
2020-09-08 22:52:50 +0200

    Create showDetails method

--- car-themed-app-frontend/src/app/components/mat-menu-button/mat-menu-button.component.ts
+++ car-themed-app-frontend/src/app/components/mat-menu-button/mat-menu-button.component.ts
@@ -1,4 +1,3 @@
-import { Component, OnInit } from '@angular/core';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
@@ -7,17 +6,28 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./mat-menu-button.component.less']
 })
 export class MatMenuButtonComponent implements OnInit {
-
+  @Input('rowId') id: number;
   @Output() valueChange = new EventEmitter();
-  edit: boolean = false;
+  @Output() displayChange = new EventEmitter();
+  detailsClick: boolean = false;
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  showId() {
+    this.detailsClick = true;
   }
 
   editRow() {
-    this.edit = true;
-    this.valueChange.emit(this.edit);
+    this.valueChange.emit(true);
   }
-  
+
+  showDetails() {
+    this.displayChange.emit(true);
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth' 
+    })
+  }
 }
