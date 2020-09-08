5131ba26f893ef87128c4ad14798cb70f978bd3c
Dorota Gil <gildota@wp.pl>
2020-09-08 15:37:00 +0200

    Close edit view

--- car-themed-app-frontend/src/app/components/details-data/details-data.component.ts
+++ car-themed-app-frontend/src/app/components/details-data/details-data.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { DealersService } from '@service/dealers.service';
+import { Router } from '@angular/router';
+import { OrdersService } from '@service/orders.service';
 
 @Component({
   selector: 'app-details-data',
   templateUrl: './details-data.component.html',
-  styleUrls: ['./details-data.component.less']
+  styleUrls: ['./details-data.component.less'],
 })
 export class DetailsDataComponent implements OnInit {
+  @Output() valueChange = new EventEmitter();
+  edit: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
+  closeEditRow() {
+    this.edit = false;
+    this.valueChange.emit(this.edit);
   }
 
 }
