8cb4f76557aa572a224a021f3ea85d9e95519356
Dorota Gil <gildota@wp.pl>
2020-09-09 22:15:53 +0200

    Display selected row details

--- car-themed-app-frontend/src/app/components/details-display/details-display.component.ts
+++ car-themed-app-frontend/src/app/components/details-display/details-display.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-details-display',
   templateUrl: './details-display.component.html',
-  styleUrls: ['./details-display.component.less']
+  styleUrls: ['./details-display.component.less'],
 })
 export class DetailsDisplayComponent implements OnInit {
+  @Input('rowData') rowData: any;
+  @Output() displayChange = new EventEmitter();
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  closeInfo() {
+    this.displayChange.emit(false);
   }
-
 }
