80653a74d1d136c70e7c835564d2f01331aad808
Dorota Gil <gildota@wp.pl>
2020-09-10 22:36:00 +0200

    Test if create Error Service

--- car-themed-app-frontend/src/app/_services/error.service.spec.ts
+++ car-themed-app-frontend/src/app/_services/error.service.spec.ts
@@ -1,16 +1,16 @@
-/* tslint:disable:no-unused-variable */
-
 import { TestBed, async, inject } from '@angular/core/testing';
 import { ErrorService } from './error.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('Service: Error', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ErrorService]
+      providers: [ErrorService],
+      imports: [HttpClientTestingModule]
     });
   });
 
-  it('should ...', inject([ErrorService], (service: ErrorService) => {
+  it('should create', inject([ErrorService], (service: ErrorService) => {
     expect(service).toBeTruthy();
   }));
 });
