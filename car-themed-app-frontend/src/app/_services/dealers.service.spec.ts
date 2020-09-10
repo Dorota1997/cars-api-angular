e37c4afb5977a59e33404edfb5c9eed10da8201a
Dorota Gil <gildota@wp.pl>
2020-09-10 21:14:10 +0200

    Test dealers method from DealersService

--- car-themed-app-frontend/src/app/_services/dealers.service.spec.ts
+++ car-themed-app-frontend/src/app/_services/dealers.service.spec.ts
@@ -1,16 +1,61 @@
-/* tslint:disable:no-unused-variable */
-
+import { IDealers, IDealer } from '@model/dealer.model';
 import { TestBed, async, inject } from '@angular/core/testing';
 import { DealersService } from './dealers.service';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
+import { environment } from 'src/environments/environment';
 
 describe('Service: Dealers', () => {
+  let httpMock: HttpTestingController;
+  let dealerService: DealersService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [DealersService]
+      providers: [DealersService],
+      imports: [HttpClientTestingModule]
     });
+
+    httpMock = TestBed.get(HttpTestingController);
+    dealerService = TestBed.get(DealersService);
   });
 
-  it('should ...', inject([DealersService], (service: DealersService) => {
+  it('should create', inject([DealersService], (service: DealersService) => {
     expect(service).toBeTruthy();
   }));
+
+  it('be able to retrieve dealers from the API bia GET', () => {
+    const dealerArray: IDealers = {
+      data: [
+        {
+          id: 60,
+          name: "Trapped, fast Jaguar engines delievery",
+          address: "17 The Drive",
+          postalCode: "TD41 8YT",
+          country: "United Kingdom"
+        },
+        {
+          id: 61,
+          name: "Tomaz Car Engines, Accessories",
+          address: "Krucza 2",
+          postalCode: "20-022",
+          country: "Poland"
+        },
+      ],
+      pageNumber: 1,
+      pageSize: 10,
+      nextPage: "https://localhost:44354/?pageNumber=2&pageSize=10",
+      previousPage: null
+    }
+
+    dealerService.dealers().subscribe((dealers) => {
+      expect(dealers.data.length).toBe(2);
+      expect(dealers).toEqual(dealerArray);
+    });
+
+    const request = httpMock.expectOne(`${environment.api}Dealers?PageNumber=1`);
+    expect(request.request.method).toBe('GET');
+    request.flush(dealerArray);
+  });
 });
