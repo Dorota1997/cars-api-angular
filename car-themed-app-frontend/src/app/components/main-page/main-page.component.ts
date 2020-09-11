cba1bc20e5f934e5038279b6c5f881530f91d378
Dorota Gil <gildota@wp.pl>
2020-09-11 16:43:31 +0200

    Dispaly dealers or orders after choose label

--- car-themed-app-frontend/src/app/components/main-page/main-page.component.ts
+++ car-themed-app-frontend/src/app/components/main-page/main-page.component.ts
@@ -6,8 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-page.component.less'],
 })
 export class MainPageComponent implements OnInit {
+  orders: boolean = false;
+  dealers: boolean = true;
   constructor() {}
 
   ngOnInit() {}
 
+  tabClick(tab) {
+    if (tab.index === 0) {
+      this.dealers = true;
+      this.orders = !this.dealers;
+    } else {
+      this.dealers = false;
+      this.orders = !this.dealers;
+    }
+  }
 }
