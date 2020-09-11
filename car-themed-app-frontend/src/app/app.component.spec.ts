d6706aa2dbe6dd49e28ea2bafe33fc494709c52e
Dorota Gil <gildota@wp.pl>
2020-09-11 18:20:59 +0200

    Remove render title test

--- car-themed-app-frontend/src/app/app.component.spec.ts
+++ car-themed-app-frontend/src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('car-themed-app-frontend');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('car-themed-app-frontend app is running!');
-  });
 });
