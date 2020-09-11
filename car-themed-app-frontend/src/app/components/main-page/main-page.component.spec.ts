254752a4a9a3c8bbc0bca7d2bb56f1526c446b42
Dorota Gil <gildota@wp.pl>
2020-09-11 18:24:13 +0200

    Test MainPage Component

--- car-themed-app-frontend/src/app/components/main-page/main-page.component.spec.ts
+++ car-themed-app-frontend/src/app/components/main-page/main-page.component.spec.ts
@@ -1,9 +1,9 @@
-/* tslint:disable:no-unused-variable */
+import { OrdersComponent } from '@components/orders/orders.component';
+import { DealersComponent } from '@components/dealers/dealers.component';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
-
-import { MainPageComponent } from './main-page.component';
+import { MainPageComponent } from '@components/main-page/main-page.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 describe('MainPageComponent', () => {
   let component: MainPageComponent;
@@ -11,9 +11,10 @@ describe('MainPageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ MainPageComponent ]
-    })
-    .compileComponents();
+      declarations: [MainPageComponent, OrdersComponent, DealersComponent],
+      imports: [HttpClientModule],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
