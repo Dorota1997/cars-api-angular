68885a7c813682c967cecea9e338e26e83d07d4b
Dorota Gil <gildota@wp.pl>
2020-09-15 15:28:47 +0200

    Remove element from row

--- car-themed-app-frontend/src/app/components/remove-modal/remove-modal.component.ts
+++ car-themed-app-frontend/src/app/components/remove-modal/remove-modal.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { DealersService } from '@service/dealers.service';
+import { OrdersService } from '@service/orders.service';
 
 @Component({
   selector: 'app-remove-modal',
@@ -7,10 +10,26 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
   styleUrls: ['./remove-modal.component.less'],
 })
 export class RemoveModalComponent implements OnInit {
-  constructor() {}
-  constructor(public dialogRef: MatDialogRef<RemoveModalComponent>,
+  constructor(
+    public dialogRef: MatDialogRef<RemoveModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public data,
+    private ordersService: OrdersService,
+    private dealersService: DealersService
+  ) {}
 
   ngOnInit() {}
+
+  remove() {
+    switch (this.data.title) {
+      case 'Orders':
+        this.ordersService.remove(this.data.id).subscribe((res: any) => {});
+        break;
+      case 'Dealers':
+        this.dealersService.remove(this.data.id).subscribe((res: any) => {});
+        break;
+    }
+  }
+
   close(): void {
     this.dialogRef.close();
   }
