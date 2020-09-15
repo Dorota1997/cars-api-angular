9de17929fef682ad28cd8c107d36438051afeb80
Dorota Gil <gildota@wp.pl>
2020-09-15 14:56:23 +0200

    Close modal

--- car-themed-app-frontend/src/app/components/remove-modal/remove-modal.component.ts
+++ car-themed-app-frontend/src/app/components/remove-modal/remove-modal.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-remove-modal',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RemoveModalComponent implements OnInit {
   constructor() {}
+  constructor(public dialogRef: MatDialogRef<RemoveModalComponent>,
 
   ngOnInit() {}
+  close(): void {
+    this.dialogRef.close();
+  }
 }
