a77d1b77e6bc08d1f2a77c92cb2a97922441c91f
Dorota Gil <gildota@wp.pl>
2020-09-16 14:53:48 +0200

    Update remove modal

--- car-themed-app-frontend/src/app/components/remove-modal/remove-modal.component.ts
+++ car-themed-app-frontend/src/app/components/remove-modal/remove-modal.component.ts
@@ -1,6 +1,5 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
-import { Router } from '@angular/router';
 import { DealersService } from '@service/dealers.service';
 import { OrdersService } from '@service/orders.service';
 
