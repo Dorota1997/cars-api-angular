2db448b24b88a7b626f31aca6e1531a637288c90
Dorota Gil <gildota@wp.pl>
2020-09-18 22:20:16 +0200

    Create forms with validatation

--- car-themed-app-frontend/src/app/components/add-element/add-element.component.ts
+++ car-themed-app-frontend/src/app/components/add-element/add-element.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-element',
   templateUrl: './add-element.component.html',
-  styleUrls: ['./add-element.component.less']
+  styleUrls: ['./add-element.component.less'],
 })
 export class AddElementComponent implements OnInit {
+  displayForms: boolean = false;
+  addDealerFormGroup: FormGroup;
 
-  constructor() { }
+  constructor() {
+    this.addDealerFormGroup = new FormGroup({
+      dealerName: new FormControl('', [Validators.required]),
+      dealerAddress: new FormControl('', [Validators.required]),
+      dealerCountry: new FormControl('', [Validators.required]),
+      dealerPostalCode: new FormControl('', [Validators.required]),
+    });
 
   ngOnInit() {
   }
 
+  ngOnInit() {}
+
+  public checkError = (controlName: string, errorName: string) => {
+    return this.addDealerFormGroup.controls[controlName].hasError(errorName);
+  };
 }
