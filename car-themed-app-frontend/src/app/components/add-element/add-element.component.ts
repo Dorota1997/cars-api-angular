ea8aee03b83a4a6dde807a40599263844e755e00
Dorota Gil <gildota@wp.pl>
2020-09-20 22:22:47 +0200

    Override formGroup and create object

--- car-themed-app-frontend/src/app/components/add-element/add-element.component.ts
+++ car-themed-app-frontend/src/app/components/add-element/add-element.component.ts
@@ -1,8 +1,9 @@
-import { SharedDataService } from './../../_services/shared-data.service';
-import { Component, OnInit } from '@angular/core';
+import { SharedDataService } from '@service/shared-data.service';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { DealersService } from '@service/dealers.service';
-import { Observable } from 'rxjs';
+import { AddDealer } from '@model/dealer.model';
+import { AddOrder } from '@model/order.model';
 
 @Component({
   selector: 'app-add-element',
@@ -10,40 +11,47 @@ import { Observable } from 'rxjs';
   styleUrls: ['./add-element.component.less'],
 })
 export class AddElementComponent implements OnInit {
-  displayForms: boolean = false;
+  @Input('formsName') formsName;
+  @Input('displayForms') displayForms;
+  @Output() showForms = new EventEmitter();
   formGroup: FormGroup;
-  formsName: string[] = [];
   title: string;
 
-  constructor(private dealerService: DealersService, private sharedDataService: SharedDataService) {
+  constructor(
+    private dealerService: DealersService,
+    private sharedDataService: SharedDataService
+  ) {
     this.sharedDataService._title.subscribe((res: any) => {
       this.title = res;
-    })
+    });
   }
 
   ngOnInit() {}
 
-  public checkError = (data: FormGroup, controlName: string, errorName: string) => {
-    return this.formGroup.controls[controlName].hasError(errorName);
+  public checkError = (
+    data: FormGroup,
+    controlName: string,
+    errorName: string
+  ) => {
+    return data.controls[controlName].hasError(errorName);
   };
 
   displayForm() {
+    this.showForms.emit(true);
     switch (this.title) {
       case 'dealers':
-        this.formsName = ['name', 'address', 'postalCode', 'country'];
         this.formGroup = new FormGroup({
           name: new FormControl('', [Validators.required]),
           address: new FormControl('', [Validators.required]),
           postalCode: new FormControl('', [Validators.required]),
-          country: new FormControl('', [Validators.required])
+          country: new FormControl('', [Validators.required]),
         });
         break;
       case 'orders':
-        this.formsName = ['components', 'orderDate', 'dealerId'];
         this.formGroup = new FormGroup({
           components: new FormControl('', [Validators.required]),
           orderDate: new FormControl('', [Validators.required]),
-          dealerId: new FormControl('', [Validators.required])
+          dealerId: new FormControl('', [Validators.required]),
         });
         break;
     }
@@ -51,18 +59,17 @@ export class AddElementComponent implements OnInit {
   }
 
   hideForm() {
+    this.showForms.emit(false);
     this.displayForms = false;
   }
 
   sendData(data: FormGroup) {
-    const obj: {[k: string]: any} = {};
+    const obj: { [k: string]: AddDealer | AddOrder } = {};
     for (var i = 0; i < this.formsName.length; i++) {
-      obj[this.formsName[i]] = data.get(this.formsName[i]).value
+      obj[this.formsName[i]] = data.get(this.formsName[i]).value;
     }
-    console.log(obj);
 
     this.dealerService.add(obj).subscribe((res: any) => {
-      console.log('Dodano nowego dealera');
       console.log(res);
     });
   }
